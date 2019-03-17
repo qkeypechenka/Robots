d00fd360223c32a8b1d7d69e83efd2451ba93fae
qkey <petrovsergy@outlook.com>
2019-03-17 18:08:35 +0500

    Add tests for logger

---
+++ robots/tests/LogWindowTests.java
@@ -0,0 +1,40 @@
+import main.java.log.LogLevel;
+import main.java.log.LogWindowSource;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+
+public class LogWindowTests {
+
+    @Test
+    void addZeroMessages() {
+        var logger = new LogWindowSource(10);
+
+        assertEquals(logger.size(), 0);
+    }
+
+    @Test
+    void addLessThanMaximumMessages() {
+        var length = 10;
+        var logger = new LogWindowSource(length);
+
+        for (var i = 0; i < length/2; i++) {
+            logger.append(LogLevel.Info, "msg");
+        }
+
+        assertEquals(logger.size(), length/2);
+    }
+
+    @Test
+    void addMoreThanMaximumMessages() {
+        var length = 10;
+        var logger = new LogWindowSource(length);
+
+        for (var i=0; i < length*2; i++) {
+            logger.append(LogLevel.Info, "msg");
+        }
+
+        assertEquals(logger.size(), length);
+    }
+}
